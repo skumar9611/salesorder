03916dcaf72a41786e318a6c2cdd76e034703161
SK <skumar9611@gmail.com>
2018-11-29 18:23:19 +0530

    Login
    Added bootstrap, added login page and 2 files to main page

---
+++ SalesOrder/src/app/components/login/login.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css','../../../../node_modules/bootstrap/dist/css/bootstrap.min.css']
+})
+export class LoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  clicked()
+  {
+  
+  }
+
+}
